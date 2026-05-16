ceeaaf62d6919128d3cdf3ad5d325fc3acf50993
HaRi-Haran-D <hvj58128@gmail.com>
2026-05-16 23:11:08 +0530

    Project Update

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,5 +9,9 @@ class CustomUserCreationForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if 'password1' in self.fields:
+            self.fields['password1'].help_text = ''
+        if 'password2' in self.fields:
+            self.fields['password2'].help_text = ''
         for field in self.fields.values():
             field.widget.attrs['class'] = 'mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-primary focus:ring-primary sm:text-sm'
